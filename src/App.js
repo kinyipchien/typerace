e8a05802c36576ac35a8e35c8cb575589cec9858
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-03 20:35:47 -0400

    Convert App into a class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TextButton from './TextButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+    this.buttonTextItems = ['Click Me!', 'No Me!', 'Please no clicky'];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Type Race</h2>
+        <input />
+        <hr />
+        {this.buttonTextItems.map((textItem) => <TextButton buttonText={textItem} />)}
+      </div>
+    );
+  }
 }
 
 export default App;
