5dad9083fc244159f1b6c730661d15ede62f005c
kinyipchien <56460213+kinyipchien@users.noreply.github.com>
2022-05-05 20:38:42 -0400

    Complete basic app

--- src/App.js
+++ src/App.js
@@ -2,24 +2,73 @@ import React from 'react';
 import './App.css';
 import TextButton from './TextButton';
 
+const buttonTextItems = [
+  'Bears, beets, battlestar galactica',
+  "What's Forrest Gump's password? 1Forrest1",
+  'Where do programmers like to hangout? The Foo Bar'];
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
-    this.buttonTextItems = ['Click Me!', 'No Me!', 'Please no clicky'];
+    this.state = { userText: '' };
+    this.textInput = React.createRef();
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h2>Type Race</h2>
-        <input />
-        <hr />
-        {this.buttonTextItems.map((textItem) => <TextButton buttonText={textItem} />)}
-      </div>
-    );
-  }
+  updateUserText = (e) => {
+    this.setState({ userText: e.target.value });
+    if (e.target.value === this.state.snippet) {
+      const endTime = new Date().getTime() - this.state.startTime;
+      this.setState({
+        highScore: (
+          this.state.highScore
+          ? Math.min(this.state.highScore, endTime)
+          : endTime
+        ),
+        victory: true,
+        endTime,
+      });
+    }
+  };
+
+  chooseSnippet = (index) => {
+    this.setState({
+      snippet: buttonTextItems[index],
+      startTime: new Date().getTime(),
+      userText: '',
+      victory: false,
+    });
+    this.textInput.current.focus();
+  };
+
+  render = () => (
+    <div className="App">
+      <h2>Type Race</h2>
+      {this.state.snippet}
+      <h4>{
+        this.state.highScore
+        ? `High Score: ${this.state.highScore}ms`
+        : null
+      }</h4>
+      <h4>{
+        this.state.victory
+        ? `Done! Woot! Time: ${this.state.endTime}ms`
+        : null
+      }</h4>
+      <input
+        value={this.state.userText}
+        onChange={this.updateUserText}
+        ref={this.textInput}
+      />
+      <hr />
+      {buttonTextItems.map((textItem, index) => (
+        <TextButton
+          key={textItem}
+          buttonText={textItem}
+          onClick={() => this.chooseSnippet(index)}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
